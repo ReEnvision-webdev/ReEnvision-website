2fedeefa1accc0b7ba21fc217d9bffb88b90eaa7
0x5b62656e5d <salt@pepper.fyi>
2025-07-09 14:59:25 +0800

    Create about page layout

---
+++ src/components/ui/image.tsx
@@ -0,0 +1,27 @@
+import NextImage from "next/image";
+import { Suspense } from "react";
+
+interface ImageProps {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+    className?: string;
+}
+
+const Image = (props: ImageProps) => {
+    return (
+        <Suspense
+            fallback={
+                <div
+                    className={`w-[${props.width}px] h-[${props.height}px] bg-gray-300 animate-pulse ${props.className}`}
+                ></div>
+            }
+        >
+            <DelayedImage src={props.src} width={props.width} height={props.height} alt={props.alt} />
+        </Suspense>
+    );
+};
+
+export default Image;
+

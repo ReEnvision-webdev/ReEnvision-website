abc69af6f2e4c1db27b95a844e0a187ac27a39b6
0x5b62656e5d <salt@pepper.fyi>
2025-07-30 11:12:31 +0800

    Fix db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,16 @@
-import { boolean, date, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { boolean, timestamp, pgTable, text, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: text().notNull().primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: text().notNull(),
   name: text().notNull(),
-  createdAt: date("created_at").notNull().defaultNow(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
   resetKey: text("reset_key"),
-  resetKeyExpires: date("reset_key_expires"),
+  resetKeyExpires: timestamp("reset_key_expires"),
   emailVerified: boolean("email_verified").notNull().default(false),
   emailVerificationKey: text("email_verification_key"),
-  emailVerificationKeyExpires: date("email_verification_key_expires"),
+  emailVerificationKeyExpires: timestamp("email_verification_key_expires"),
   isAdmin: boolean("is_admin").notNull().default(false),
   isBanned: boolean("is_banned").notNull().default(false),
 });

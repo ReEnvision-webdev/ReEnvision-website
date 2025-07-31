f1abd44f6829c1542391f8778cfdcb99b53bf0b2
Subham <subham.work19181@gmail.com>
2025-07-31 09:43:02 +0530

    improve responsive layout for contact info
    the contact page to enhance its responsiveness. The left section, containing contact details, is now centrally aligned on small and medium screens for better visual presentation and user experience on mobile devices.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ export default function Footer() {
   return (
     <footer className="bg-[#1d588a] text-[#F0F8FF] py-8">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-3 gap-8 items-center">
-          <div className="flex items-center space-x-2">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
+          <div className="hidden md:flex items-center space-x-2">
             <Image
               src="/vectors/favicon.svg" // Path to your favicon.png
               alt="Logo"
-              width={32} // Specify the width (in pixels)
-              height={32} // Specify the height (in pixels)
-              className="h-10 w-10" // Optional: Add any additional styles
+              width={50} // Specify the width (in pixels)
+              height={50} // Specify the height (in pixels)
+              className="h-15 w-15" // Optional: Add any additional styles
             />
           </div>
           <div className="text-center">
@@ -24,7 +24,7 @@ export default function Footer() {
               &#169; {currentYear} ReEnvision. All rights reserved.
             </p>
           </div>
-          <div className="text-right">
+          <div className="text-center md:text-right">
             <Link href="/contact">
               <Button
                 variant="outline"

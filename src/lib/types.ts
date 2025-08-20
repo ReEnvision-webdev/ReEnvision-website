5c238c46fed9b48e7b29026fed96c2192cf1f026
0x5b62656e5d <salt@pepper.fyi>
2025-08-20 08:01:19 +0800

    Update standard response interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,6 @@
 export interface StandardResponse {
   success: boolean;
   message: string | null;
-  data: Record<string, unknown> | null;
+  data: Record<string, unknown> | Array<Record<string, unknown>> | null;
   error: string | null;
 }

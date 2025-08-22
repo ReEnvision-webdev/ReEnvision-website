9c97a4b2cae429f98e122984176af033b9a1536d
0x5b62656e5d <salt@pepper.fyi>
2025-08-22 10:40:23 +0800

    Fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const supabaseHost = new URL(`${process.env.NEXT_PUBLIC_SUPABASE_URL as string}/**`);
+
 const nextConfig: NextConfig = {
   devIndicators: false /* config options here */,
+  images: {
+    remotePatterns: [supabaseHost],
+  },
 };
 
 export default nextConfig;

82a8114fd6f873e3178d47b87027c4088203180f
NEOLabs-software <THAYEEBOI890@GMAIL.COM>
2025-08-24 14:40:13 -0500

    lint again idk

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
-const supabaseHost = new URL(
-  `${process.env.NEXT_PUBLIC_SUPABASE_URL as string}/**`,
-);
-
 const nextConfig: NextConfig = {
   devIndicators: false /* config options here */,
   images: {
-    remotePatterns: [supabaseHost],
+    remotePatterns: process.env.NEXT_PUBLIC_SUPABASE_URL
+      ? [
+          {
+            hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
+            pathname: "/**",
+          },
+        ]
+      : [],
   },
 };
 

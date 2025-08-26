5d57cab9edabfb6e0f487d07b7ddb220387fac55
NEOLabs-software <THAYEEBOI890@GMAIL.COM>
2025-08-25 20:04:11 -0500

    fix a single styling issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
     return (
       <div className="container mx-auto p-6 py-25">
         <div className="mb-8">
-          <h1 className="text-3xl font-bold text-blue-800">Admin Dashboard</h1>
+          <h1 className="text-3xl font-bold text-[#1f639e]">Admin Dashboard</h1>
           <p className="text-muted-foreground">
             Welcome, administrator! You have special access privileges.
           </p>

53ea26b7feeb82e33107ba29465ed7350bd56fcf
thayeeboi890 <thayeeboi890@gmail.com>
2025-08-27 18:08:39 +0000

    change to eslint to check for unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:import/recommended",
+  ),
+  {
+    rules: {
+      "import/order": "off",
+    },
+  },
 ];
 
 export default eslintConfig;

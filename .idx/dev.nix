10a88a468511c92b7580e46c6b6979bd3c1c5380
thayeeboi890 <thayeeboi890@gmail.com>
2026-01-28 13:47:48 +0000

    attempt 1 fix error

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,12 +5,12 @@
     pkgs.gh
   ];
   idx.extensions = [
-    
-  
- "ahmadawais.shades-of-purple"
- "Google.validation-agent-extension"
- "Letgamer.sweet-vscode-icons"];
+    "ahmadawais.shades-of-purple"
+    "Google.validation-agent-extension"
+    "Letgamer.sweet-vscode-icons"
+  ];
   idx.previews = {
+    enable = true; # Required to activate the previews block
     previews = {
       web = {
         command = [
@@ -25,4 +25,14 @@
       };
     };
   };
+  
+
+  # Standard environment variables go here. 
+  # If you don't specifically need 'currentNodeJsVersion' for your app code, remove it.
+  env = {
+  # Add this specific field if the Studio backend is looking for it
+  currentNodeJsVersion = "20"; 
+  NODE_VERSION = "20";
+};
+
 }
